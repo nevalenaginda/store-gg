0542039258235da2c1dba4cbf03ef9e4d4cce137
Nevalen Aginda Prasetyo <nevalen.prasetyo@btpn.com>
2021-12-30 21:09:39 +0700

    slicing checkout page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/detail.css';
+import '../styles/sign-in.css';
 import '../styles/homepage.css';
+import '../styles/checkout.css';
 import '../styles/utilities.css';
+import '../styles/complete-checkout.css';
 import 'aos/dist/aos.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -22,12 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
-        {/* AOS Animation */}
-        {/* <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" /> */}
-
         <title>StoreGG</title>
-
         {/* Call Bootstrap */}
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
