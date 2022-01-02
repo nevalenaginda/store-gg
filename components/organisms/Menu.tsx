5179f67f1ff93751bc0471646d8bd1341bcdcce0
Nevalen Aginda Prasetyo <nevalen.prasetyo@btpn.com>
2022-01-03 02:16:15 +0700

    Refactor Navbar

---
+++ components/organisms/Menu.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import Link from 'next/link';
+
+interface MenuProps {
+    title: string,
+    active?: boolean,
+    href: string
+  }
+
+function Menu(props: Partial<MenuProps>) {
+  const { title, active, href = '/' } = props;
+  const classTitle = classNames({
+    'nav-link': true,
+    active,
+  });
+
+  return (
+    <li className="nav-item my-auto">
+      <Link href={href}>
+        <a className={classTitle} aria-current="page">{title}</a>
+      </Link>
+    </li>
+  );
+}
+
+export default Menu;
